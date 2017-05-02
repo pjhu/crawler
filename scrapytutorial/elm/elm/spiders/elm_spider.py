0a4afd9eb385db6be90e962855041cd1edf38707
Jinhu Peng <jhpeng@thoughtworks.com>
2017-05-02 16:47:05 +0800

    scrapy: get all data from all urls

--- scrapytutorial/elm/elm/spiders/elm_spider.py
+++ scrapytutorial/elm/elm/spiders/elm_spider.py
@@ -8,13 +8,12 @@ class ElmSpider(scrapy.Spider):
     name = "elm"
 
     def start_requests(self):
-        urls = [
-            "https://mainsite-restapi.ele.me/shopping/restaurants?"
-            "extras%5B%5D=activities&geohash=wtw3dfs7rep&"
-            "latitude=31.21548&limit=24&longitude=121.41608&offset=0&terminal=web",
-        ]
+        url_no_offset = "https://mainsite-restapi.ele.me/shopping/restaurants" \
+                        "?extras%5B%5D=activities&geohash=wtw3dfs7rep&latitude=31.21548" \
+                        "&limit=24&longitude=121.41608&offset="
 
-        for url in urls:
+        for offset in range(100):
+            url = "".join([url_no_offset, str(offset*24), "&terminal=web"])
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
